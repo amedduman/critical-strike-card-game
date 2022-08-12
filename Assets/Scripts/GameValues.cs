e4f06f956c25158180c0713c475ae64b3ca04e67
amed <amedduman@protonmail.com>
2022-08-12 03:21:53 +0300

    Add game values

--- Assets/Scripts/GameValues.cs
+++ Assets/Scripts/GameValues.cs
@@ -5,15 +5,5 @@ using Sirenix.OdinInspector;
 public class GameValues : ScriptableObject
 {
     public static int WheelSliceCount {get; private set;} = 8;
-    public static Sprite DeathSprite {get; private set;}
-
-    [ReadOnly] [SerializeField] int _wheelSliceCount = 8;
-    [SerializeField] Sprite _deathSprite;
-
-    void OnValidate()
-    {
-        WheelSliceCount = _wheelSliceCount;
-        DeathSprite = _deathSprite;
-    }
-    
+    [field: SerializeField] public GameObject DeathDrop {get; private set;}
 }
--- Assets/Scripts/WheelContent.cs
+++ Assets/Scripts/WheelContent.cs
@@ -2,29 +2,25 @@ namespace CardGame
 {
     using System;
     using UnityEngine;
-    using UnityEngine.UI;
 
     [CreateAssetMenu(fileName = "wheel_content", menuName = "CardGame/WheelContent")]
     public class WheelContent : ScriptableObject
     {
+        [SerializeField] GameValues _gameValues;
         public WheelContentData[] Slices = new WheelContentData[GameValues.WheelSliceCount];
 
         void OnValidate()
         {
             if (Slices.Length != GameValues.WheelSliceCount) 
             {
-                Array.Resize(ref Slices, GameValues.WheelSliceCount);
+                Array.Resize(ref Slices, GameValues.WheelSliceCount); 
             }
 
             for(int i = 0; i < Slices.Length; i++)  
             {
-                if(Slices[i].Drop == null) return;
+                if(Slices[i].Drop == null) return; 
 
-                Sprite dropSprite = Slices[i].Drop.GetComponent<Image>().sprite;
-
-                if(dropSprite == null) return;
-
-                if(dropSprite.name == GameValues.DeathSprite.name)  
+                if(Slices[i].Drop.name == _gameValues.DeathDrop.name)   
                 {
                     Slices[i].IsDeath = true;
                 }
