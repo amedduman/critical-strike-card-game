e8af799c48b2ffdd771ed44b47cc543daf6135ce
amed <amedduman@protonmail.com>
2022-09-08 00:36:23 +0300

    Final changes

--- Assets/Scripts/WheelContentData.cs
+++ Assets/Scripts/WheelContentData.cs
@@ -10,12 +10,15 @@ namespace CardGame
     [System.Serializable]
     public class WheelContentData
     {
+#if UNITY_EDITOR
         [ValueDropdown(nameof(GetAllDropPrefabs), IsUniqueList = true)]
+#endif
         public GameObject Drop;
         [Range(0, 25)] public int DropCount = 1;
         [Range(0, 1)] public float DropRate = 1;
-        [ReadOnly] [SerializeField] public bool IsDeath;
+        [ReadOnly][SerializeField] public bool IsDeath;
 
+#if UNITY_EDITOR
         private static IEnumerable GetAllDropPrefabs()
         {
             var root = "Assets/Game/Prefabs/Drops/";
@@ -25,6 +28,7 @@ namespace CardGame
                 .Select(x => x.Substring(root.Length))
                 .Select(x => new ValueDropdownItem(x, UnityEditor.AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(root + x)));
         }
+#endif
     }
 
 }
